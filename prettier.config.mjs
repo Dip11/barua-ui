e3b2766064aad017ffd9b8f16b996d39aaa3ac24
Dipta Barua <diptab11@gmail.com>
2026-05-12 12:50:42 +0900

    chore: add editorconfig and prettier setup

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: "es5",
+  semi: false,
+  arrowParens: "avoid",
+};
